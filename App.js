40546a714ca9a7f0583dd28537467548fe92de46
Nadila JM <96558056+nadilajm@users.noreply.github.com>
2023-09-29 14:36:44 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+let Name = "Nadila Jannatul Ma'wa";
+let NIM = "20220040199";
+let number1 = 9;
+let number2 = 2;
+let numbers = number1*number2;
+
+console.log("Nama : " + Name);
+console.log("NIM : " + NIM);
+console.log("hasilnya adalah : " + numbers);
