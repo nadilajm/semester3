ee39d47ab444170842214e3ad11a792817f23241
Nadila JM <96558056+nadilajm@users.noreply.github.com>
2023-10-04 18:27:59 +0700

    Add files via upload

---
+++ sesi2.js
@@ -0,0 +1,11 @@
+let firstName = "Nadila"
+let lastName = "jannatul"
+let age = 20
+let isMarried = false
+
+console.log("my firstname :" + firstName)
+console.log("my last name :" + lastName)
+console.log("my age :" + age)
+console.log("is it true that  i am married ? " +isMarried)
+
+
