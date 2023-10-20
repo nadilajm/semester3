320426bcd52c05bfb2dd69e24563e003c8ed6bf1
Nadila JM <96558056+nadilajm@users.noreply.github.com>
2023-10-20 20:03:01 +0700

    Add files via upload

---
+++ sesi3.js
@@ -0,0 +1,27 @@
+// No.1 
+var restaurant = {
+    name: "NJ",
+    city: "Luxembergh",
+    favoriteDrink: "Mineral Water",
+    favoriteFood: "Seblak",
+    isVegan: false 
+};
+
+// No.2
+var restaurant = [
+    {
+        name: "JM",
+        city: "Korea",
+        favoriteDrink: "Soju",
+        favoriteFood: "Kimbap",
+        isVegan: true
+    },
+    {
+        name: "NJM",
+        city: "Indonesisa",
+        favoriteDrink: "Milk",
+        favoriteFood: "Noodle",
+        isVegan: false
+    },
+    
+];
